e7e13cd91a9c12a61f871490edb87fdf7c9321ac
mugicha9 <jkn.mugicha@gmail.com>
2023-03-18 19:07:17 +0900

    implment NavigationBar

--- lib/main.dart
+++ lib/main.dart
@@ -19,27 +19,60 @@ class MainApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({
     super.key,
   });
 
   @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var selectedIndex = 0;
+
+  @override
   Widget build(BuildContext context) {
+    Widget appPage;
+    switch (selectedIndex) {
+      case 0:
+        appPage = RecipePage();
+        break;
+      case 1:
+        appPage = MakePage();
+        break;
+      case 2:
+        appPage = SettingPage();
+        break;
+      default:
+        throw UnimplementedError("No widget for index $selectedIndex");
+    }
     return Scaffold(
-      body: Row(
+      body: Column(
         children: [
-          const Expanded(
-            child: Text('Hello World!'),
+          Expanded(
+            child: appPage,
           ),
           SafeArea(
               child: NavigationBar(
             destinations: const [
-              NavigationDestination(icon: Icon(Icons.coffee), label: 'Recipe'),
-              NavigationDestination(icon: Icon(Icons.book), label: 'Make'),
               NavigationDestination(
-                  icon: Icon(Icons.settings), label: 'Setting'),
+                  icon: Icon(Icons.book_outlined),
+                  selectedIcon: Icon(Icons.book),
+                  label: 'Recipe'),
+              NavigationDestination(
+                  icon: Icon(Icons.coffee_outlined),
+                  selectedIcon: Icon(Icons.coffee),
+                  label: 'Make'),
+              NavigationDestination(
+                  icon: Icon(Icons.settings_outlined),
+                  selectedIcon: Icon(Icons.settings),
+                  label: 'Setting'),
             ],
+            selectedIndex: selectedIndex,
+            onDestinationSelected: (value) => setState(() {
+              selectedIndex = value;
+            }),
           ))
         ],
       ),
@@ -47,6 +80,43 @@ class MyHomePage extends StatelessWidget {
   }
 }
 
-class MainStatus extends ChangeNotifier {
-  var selectedIndex = 0;
+class SettingPage extends StatelessWidget {
+  const SettingPage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Setting'),
+    );
+  }
 }
+
+class MakePage extends StatelessWidget {
+  const MakePage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Make'),
+    );
+  }
+}
+
+class RecipePage extends StatelessWidget {
+  const RecipePage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Recipe'),
+    );
+  }
+}
+
+class MainStatus extends ChangeNotifier {}
